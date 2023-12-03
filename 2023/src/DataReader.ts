6f10c4a962af6fb211a453a177aaf42393758310
Ross Tinsley <rtinsley1199@gmail.com>
2023-12-03 18:50:33 +0000

    Setup 2023

---
+++ 2023/src/DataReader.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+
+export function readDataRaw(dayNumber: number): string {
+  try {
+    return fs.readFileSync(
+      `/Users/rosstinsley/Documents/Personal/Advent-of-Code/2023/data/Day_${dayNumber}.txt`,
+      'utf8',
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function readDataArray(dayNumber: number): string[] {
+  return readDataRaw(dayNumber).split('\n');
+}
